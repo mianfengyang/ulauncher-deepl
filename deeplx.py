d4fb5342efcf0bb94adf0db73e22e27ccbeb55e7
mianfengyang <181500944@qq.com>
2024-07-30 18:50:31 +0800

    local program

--- deeplx.py
+++ deeplx.py
@@ -1,30 +1,32 @@
 import httpx, json
 
-data = {
-	"text": "helo my sister",
-}
-data = {
-	"text": "hello my sister",
-	"source": "auto",
-	"target_lang": "zh",
-}
-input = "tr zh:en 你好漂亮"
-input = "tr zh:en 你好美"
-data = {
-	"text": input.split(" ")[2],
-	"source_lang": input.split(" ")[1].split(":")[0].upper(),
-	"target_lang": input.split(" ")[1].split(":")[1].upper(),
-}
-post_data = json.dumps(data)
 
-def translation(query):
+def translation():
     deeplx_api = "http://192.168.1.7:1188/translate"
-    rt = httpx.post(url = deeplx_api, data = query).json()
-    print(rt)
-    return rt
+    
+    content = input("eg. en:zh hello: ")
+    while content != "q":
+        #data = {
+        #    "text": content.split(" ")[1],
+        #    "source_lang": content.split(" ")[0].split(":")[0].upper(),
+        #    "target_lang": content.split(" ")[0].split(":")[1].upper(),
+        #}
+        data = {
+            "text": content.split(" ")[1],
+            "source_lang": "auto",
+            "target_lang": content.split(" ")[0].split(":")[1].upper(),
+        }
+        query = json.dumps(data)
+        rt = httpx.post(url = deeplx_api, data = query).json()
+        if rt["alternatives"] != None:
+            rt["alternatives"].append(rt["data"])
+            for i in rt["alternatives"]:
+                print(i)
+        else:
+            print(rt["data"])
+        content = input("eg. en:zh hello: ")
+
 
 if __name__ == '__main__':
-    r = translation(post_data)
-    for i in r["alternatives"]:
-        print(i)
-    print(r["data"] )
+    translation()
+  
